906e05d9a85b69db9226164b26d61276595e3a64
Isra <pirravk@gmail.com>
2022-11-14 00:21:57 +0100

    añado sonido a la calculadora

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		CalculatorFrame frame = new CalculatorFrame();
 		EventsController control = new EventsController(frame);
+		control.PlaySound("sounds/R2D2.wav");
 		frame.controller(control);
 
 	}
